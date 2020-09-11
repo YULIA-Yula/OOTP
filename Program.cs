b2ce1d45f7b8150447d6fc4c51dd09fa7d0324a5
YULIA-Yula <71152759+YULIA-Yula@users.noreply.github.com>
2020-09-11 23:15:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("work with Git");
+            Console.WriteLine("Synchronization VS and GitHub");
+        }
+    }
+}
